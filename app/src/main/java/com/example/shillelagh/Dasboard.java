548c4549a03979ed4fb1f9e9ed18da5caed3216b
Premalatha <premanaidu22d@gmail.com>
2021-04-05 00:53:07 +0530

    Dashboard

--- app/src/main/java/com/example/shillelagh/Dasboard.java
+++ app/src/main/java/com/example/shillelagh/Dasboard.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Dashboard extends AppCompatActivity {
+public class Dasboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
